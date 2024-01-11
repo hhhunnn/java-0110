d8850ee3498c68ae598d039a04e928abf3ec678c
hhhunnn <gusdk2090@naver.com>
2024-01-11 17:48:58 +0900

    java-240110

---
+++ JAVA_bt1228/src/c_Chapter02/D_While.java
@@ -0,0 +1,18 @@
+package c_Chapter02;
+
+// while 문
+// : 일반적으로 반복 횟수가 지정되지 않을 때 사용하는 반복문
+
+// 조건을 검사하고 해당 조건이 true 인 경우 코드 블럭을 실행하는 반복문
+// : if문의 반복 형태
+
+public class D_While {
+
+	public static void main(String[] args) {
+		
+		// 기본 구조
+		
+
+	}
+
+}
